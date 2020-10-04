b008f34d46410396b87c9e186cec938198de8a72
tabatamasahiro <tbtmshr2@gmail.com>
2020-10-04 20:56:33 +0900

    メソッド[削除By著者名/出版日の変更/Titleの変更/著者名で検索にOradeBy出版日]を追加

--- src/main/kotlin/com/example/tabata/repository/BookRepository.kt
+++ src/main/kotlin/com/example/tabata/repository/BookRepository.kt
@@ -1,9 +1,11 @@
 package com.example.tabata.repository
 
 import com.example.tabata.domain.Book
+import io.micronaut.data.annotation.Id
 import io.micronaut.data.jdbc.annotation.JdbcRepository
 import io.micronaut.data.model.query.builder.sql.Dialect
 import io.micronaut.data.repository.CrudRepository
+import java.time.LocalDate
 import java.util.*
 
 @JdbcRepository(dialect = Dialect.H2)
@@ -11,6 +13,12 @@ interface BookRepository : CrudRepository<Book, UUID> {
 
     override fun findAll(): List<Book>
 
-    fun findByAuthorName(authorName: String): Optional<Book>
+    fun deleteByAuthorName(authorName: String)
+
+    fun findByAuthorNameOrderByDatePublication(authorName: String): List<Book>
+
+    fun update(@Id isbn: UUID?, datePublication: LocalDate)
+
+    fun update(@Id isbn: UUID?, title: String)
 
 }
