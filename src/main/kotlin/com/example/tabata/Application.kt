e1f9ae681d25e6ba71f4f80ea788d942e1aa220a
tabatamasahiro <tbtmshr2@gmail.com>
2020-10-05 22:45:41 +0900

    Release.ktをBook.ktから移動した

--- src/main/kotlin/com/example/tabata/Application.kt
+++ src/main/kotlin/com/example/tabata/Application.kt
@@ -1,6 +1,7 @@
 package com.example.tabata
 
 import com.example.tabata.domain.Book
+import com.example.tabata.domain.Release
 import com.example.tabata.repository.BookRepository
 import io.micronaut.context.event.StartupEvent
 import io.micronaut.core.annotation.TypeHint
@@ -8,7 +9,6 @@ import io.micronaut.runtime.Micronaut.*
 import io.micronaut.runtime.event.annotation.EventListener
 import org.slf4j.LoggerFactory
 import java.time.LocalDate
-import java.time.ZoneId
 import java.util.*
 import javax.inject.Singleton
 
@@ -32,10 +32,10 @@ class Application(
             LOG.info("Populating data")
         }
 
-        var book1 = Book(UUID.randomUUID(), "落合陽一", "日本再興戦略", Book.Release.DONE,
+        var book1 = Book(UUID.randomUUID(), "落合陽一", "日本再興戦略", Release.DONE,
                 LocalDate.of(2018, 1, 31))
 
-        var book2 = Book(UUID.randomUUID(), "安宅和人", "issue driven", Book.Release.DONE,
+        var book2 = Book(UUID.randomUUID(), "安宅和人", "issue driven", Release.DONE,
                 LocalDate.of(2010, 11, 24))
 
         bookRepository.saveAll(listOf(book1, book2))
