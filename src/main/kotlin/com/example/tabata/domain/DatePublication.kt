e2b1b4ccd530afd0466a72f01082090a277ee375
tabatamasahiro <tbtmshr2@gmail.com>
2020-10-05 22:11:44 +0900

    出版日を更新する場合の制御とそのTESTを作成

---
+++ src/main/kotlin/com/example/tabata/domain/DatePublication.kt
@@ -0,0 +1,62 @@
+package com.example.tabata.domain
+
+import java.lang.Exception
+import java.time.DateTimeException
+import java.time.LocalDate
+import java.time.ZoneId
+
+class DatePublication(val yyyymmdd: String) {
+    companion object {
+        fun valueToUpdate(yyyymmdd: String): DatePublication {
+            return DatePublication(yyyymmdd)
+        }
+
+        fun isPastDate(yyyymmdd: LocalDate): Boolean {
+            return now().isAfter(yyyymmdd)
+        }
+
+        fun now() = LocalDate.now(ZoneId.of("Asia/Tokyo"))
+    }
+
+    fun parseByRelease(release: Book.Release): Update {
+
+        if (isAlready(release)) {
+            // 出版済:出版日更新NG
+            return Update.NG
+        }
+
+        // 出版まだ
+        if (undecided()) {
+            // 出版日:未定
+            return Update.OK
+        }
+
+        try {
+            return parse()
+        } catch (e: DateTimeException) {
+            return Update.NG
+        }
+    }
+
+    fun isAlready(release: Book.Release): Boolean {
+        return release == Book.Release.DONE
+    }
+
+    fun undecided(): Boolean {
+        return yyyymmdd.isNullOrBlank()
+    }
+
+    fun parse(): Update {
+        if (isPastDate(LocalDate.parse(yyyymmdd))) {
+            //昨日以前の日付へ変更する事はできないためNG
+            return Update.NG
+        } else {
+            return Update.OK
+        }
+    }
+
+    enum class Update {
+        OK, NG,
+    }
+
+}
---
+++ src/test/kotlin/com/example/tabata/domain/DatePublicationTest.kt
@@ -0,0 +1,84 @@
+package com.example.tabata.domain
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import java.time.LocalDate
+
+internal class DatePublicationTest {
+
+    @Test
+    fun 現在は過去ではない() {
+
+        var now = DatePublication.now()
+
+        println(now)
+
+        assertFalse(DatePublication.isPastDate(now))
+    }
+
+    @Test
+    fun 現在minus1は過去() {
+
+        var now = DatePublication.now()
+        var minus1Days = now.minusDays(1)
+
+        println(minus1Days)
+
+        assertTrue(DatePublication.isPastDate(minus1Days))
+    }
+
+    @Test
+    fun 現在Plus1は過去日ではない() {
+
+        var now = DatePublication.now()
+        var plusDays1 = now.plusDays(1)
+
+        println(plusDays1)
+
+        assertFalse(DatePublication.isPastDate(plusDays1))
+    }
+
+
+    @Test
+    fun 未出版で出版日は現在日の場合UpdateOK() {
+
+        var now = DatePublication.now().toString()
+
+        var parseByRelease = DatePublication.valueToUpdate(now).parseByRelease(Book.Release.NOT_DONE)
+
+        assertEquals(parseByRelease, DatePublication.Update.OK)
+    }
+
+    @Test
+    fun 未出版で出版日は現在日以降の場合UpdateOK() {
+
+        var plus1Days = DatePublication.now().plusDays(1).toString()
+
+        var parseByRelease =
+                DatePublication.valueToUpdate(plus1Days).parseByRelease(Book.Release.NOT_DONE)
+
+        assertEquals(parseByRelease, DatePublication.Update.OK)
+    }
+
+
+    @Test
+    fun 未出版で出版日は過去日の場合UpdateNG() {
+
+        var minusDay1 = DatePublication.now().minusDays(1)
+
+        var parseByRelease =
+                DatePublication.valueToUpdate(minusDay1.toString()).parseByRelease(Book.Release.NOT_DONE)
+
+        assertEquals(parseByRelease, DatePublication.Update.NG)
+    }
+
+    @Test
+    fun 出版済の場合はUpdateNG() {
+        var now = DatePublication.now().toString()
+
+        var parseByRelease = DatePublication.valueToUpdate(now).parseByRelease(Book.Release.DONE)
+
+        assertEquals(parseByRelease, DatePublication.Update.NG)
+    }
+}
